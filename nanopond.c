1b006599abad63c39fcf67d1e02218a9607ff779
Dylan River Callaghan Chapell <d_chapell@coloradocollege.edu>
2023-12-07 09:45:56 -0800

    turn on threading

--- nanopond.c
+++ nanopond.c
@@ -258,7 +258,7 @@
 // #define USE_SDL 1
 
 /* Define this to use threads, and how many threads to create */
-// #define USE_PTHREADS_COUNT 4
+#define USE_PTHREADS_COUNT 4
 
 /* ----------------------------------------------------------------------- */
 
